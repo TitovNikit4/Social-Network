93a0904fddcd9f060b099728287fe415c5fcdc39
Nikita Titov <tns36reg@gmail.com>
2022-02-22 20:35:52 +0300

    Split by components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src='https://mpng.subpng.com/20180519/rya/kisspng-logo-phoenix-art-5afffc96998f80.692522331526725782629.jpg' />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://thumbs.dreamstime.com/b/d-mural-wallpaper-beautiful-view-landscape-background-old-arches-tree-sun-water-birds-flowers-transparent-curtains-166191190.jpg' />
+      </div>
+      <div>
+        ava + decription
+      </div>
+      <div>
+        My Posts
+        <div>
+          New Post
+        </div>
+        <div>
+          <div>
+            Post 1
+          </div>
+          <div>
+            Hey Post 1
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
