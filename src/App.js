34ca187527f456c8d3054d20c4b0902135f7d524
Nikita Titov <tns36reg@gmail.com>
2022-02-22 19:48:52 +0300

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import logo from './logo.svg';
-import Technologies from './Technologies.js';
-import Header from './Header.js';
 
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://mpng.subpng.com/20180519/rya/kisspng-logo-phoenix-art-5afffc96998f80.692522331526725782629.jpg' />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://thumbs.dreamstime.com/b/d-mural-wallpaper-beautiful-view-landscape-background-old-arches-tree-sun-water-birds-flowers-transparent-curtains-166191190.jpg' />
+        </div>
+        <div>
+          ava + decription
+        </div>
+        <div>
+          My Posts
+          <div>
+            New Post
+          </div>
+          <div>
+            <div>
+              Post 1
+            </div>
+            <div>
+              Hey Post 1
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
