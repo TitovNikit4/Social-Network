1a9860c008836e9ecd00092ccb959f0f0abcf71b
Nikita Titov <tns36reg@gmail.com>
2022-02-23 02:50:39 +0300

    Css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import s from './Header.module.css';
+
 const Header = () => {
   return (
-    <header className='header'>
+    <header className={s.header}>
       <img src='https://mpng.subpng.com/20180519/rya/kisspng-logo-phoenix-art-5afffc96998f80.692522331526725782629.jpg' />
     </header>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,8 @@
+import s from './Profile.module.css';
+
 const Profile = () => {
   return (
-    <div className='content'>
+    <div className={s.content}>
       <div>
         <img src='https://thumbs.dreamstime.com/b/d-mural-wallpaper-beautiful-view-landscape-background-old-arches-tree-sun-water-birds-flowers-transparent-curtains-166191190.jpg' />
       </div>
@@ -12,11 +14,11 @@ const Profile = () => {
         <div>
           New Post
         </div>
-        <div>
-          <div>
+        <div className='posts'>
+          <div className={s.item}> 
             Post 1
           </div>
-          <div>
+          <div className={s.item}>
             Hey Post 1
           </div>
         </div>
